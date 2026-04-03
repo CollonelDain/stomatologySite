656c5a121185beb99869d0846f8359163cc7cc4c
CollonelDain <d4nillpopoff@yandex.ru>
2026-04-03 16:35:58 +0300

    Add base frontend part for accounts

--- config/settings.py
+++ config/settings.py
@@ -152,8 +152,8 @@ SPECTACULAR_SETTINGS = {
 
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Укажите порт, на котором работает ваш Vue.js
-    "http://127.0.0.1:5173",
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
 ]
 
 # JWT Configuration
